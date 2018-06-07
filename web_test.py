50ef64cd0fbd63847a5ad8cb8afc45e81749b6d6
YiCheng <yichengace@hotmail.com>
2018-06-07 19:00:34 +0800

    0607_2
    添加了mac上的chromedriver位置

--- web_test.py
+++ web_test.py
@@ -1,7 +1,9 @@
 #encoding utf-8
 from selenium import webdriver
 
-chromdriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+# chromdriver_win = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+chromdriver_macos ="/Applications/chromedriver" 
 
-driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+# driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+driver = webdriver.Chrome(chromdriver_macos)
 driver.get("http://www.baidu.com")
