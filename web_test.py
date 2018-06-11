bd170b14996f1e8b5d498e9fa786cedb9755852b
YiCheng <yichengace@hotmail.com>
2018-06-11 15:45:01 +0800

    0611_1
    mac这边也提交一下代码

--- web_test.py
+++
@@ -1,9 +0,0 @@
-#encoding utf-8
-from selenium import webdriver
-
-# chromdriver_win = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
-chromdriver_macos ="/Applications/chromedriver" 
-
-# driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
-driver = webdriver.Chrome(chromdriver_macos)
-driver.get("http://www.baidu.com")
