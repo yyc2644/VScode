c1006e35e87780986ca3309cd21080d0b251aab1
yyc2644 <yichengace@hotmail.com>
2018-08-01 13:58:51 +0800

    0801
    没有做任何改动

---
+++ timechange.py
@@ -0,0 +1,21 @@
+#author:yicheng
+#用来转换unix时间戳
+
+import time
+
+# t = time.time()
+
+#输入时间戳
+# timeStamp = 1532066640
+
+# #解析
+# timeArray = time.localtime(timeStamp)
+# print(timeStamp)
+# #字符串拼接
+# StyleTime = time.strftime("%Y-%m-%d %H:%M:%S:%S:%S")
+
+# print(StyleTime)
+
+# timeStamp= time.time()
+# print(timeStamp)
+# print(time.gmtime(timeStamp))
